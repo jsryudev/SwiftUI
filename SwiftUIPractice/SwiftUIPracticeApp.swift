91250bd50db369b06a86b5022852c17a45c27968
jsryudev <jsryu.dev@gmail.com>
2021-10-11 22:16:17 +0900

    Init project

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by JunSang Ryu on 2021/10/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
